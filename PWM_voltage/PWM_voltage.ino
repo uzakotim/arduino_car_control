4aa5453a591f3b94230da46fd6163f990c19d6d3
uzakotim <timauzakov@gmail.com>
2024-10-24 10:02:22 +0500

    PWM speed control

---
+++ PWM_voltage/PWM_voltage.ino
@@ -0,0 +1,15 @@
+const int transistorPin = 9;
+int dutyCycle = 50;
+int period = 1000;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(transistorPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(transistorPin, HIGH);
+  delay(dutyCycle * period / 100);
+  digitalWrite(transistorPin, LOW);
+  delay((100-dutyCycle) * period / 100);
+}
