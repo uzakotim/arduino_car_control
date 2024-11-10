a40f4c8b6f012116847c1aa5a751b3b589d21fd3
uzakotim <timauzakov@gmail.com>
2024-11-10 13:48:51 +0500

    measuring current

--- window_opener_switches/window_opener_switches.ino
+++ window_opener_switches/window_opener_switches.ino
@@ -4,18 +4,19 @@ const int relayA = 8;
 const int relayB = 9;  
 const int currentSensorPin = A0;
 const int WINDOW_SIZE = 10;
+const int DELAY_OBSTACLE_LOWERING_WINDOW = 500;
+const float MAX_CURRENT = 30.0; //Amps
+const float SLOPE_THRESHOLD = 2.0;
 
 int stateUP = 0;
 int stateDOWN = 0;
-char command = 's';
-char prev_command = 's'; 
-const float MAX_CURRENT = 30.0; //Amps
+int counter = 0;
 float current = 0.0;
 float voltage = 0.0;
-float nominal_current = 0.0;
-int DELAY_OBSTACLE_LOWERING_WINDOW = 500;
 float current_readings[WINDOW_SIZE];
-int counter = 0;
+
+char command = 's';
+char prev_command = 's'; 
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,6 +29,9 @@ void setup() {
   // Убедитесь что реле выключены во время стартапа
   digitalWrite(relayA, LOW);
   digitalWrite(relayB, LOW);
+  for (int i=0;i<WINDOW_SIZE;i++){
+    current_readings[i] = 0;
+  }
   Serial.begin(9600);
 
 }
@@ -40,52 +44,55 @@ void loop() {
   voltage = analogRead(currentSensorPin);
   current = (voltage/ 1024.0) * MAX_CURRENT;
   addCurrentReading(current);
+  for (int i=0;i<WINDOW_SIZE;i++){
+    Serial.print(current_readings[i]); 
+    Serial.print(" ");
+  }
+  Serial.print("\n");
   // Комманда с компьютера
   if (Serial.available() > 0)
   {
     command = Serial.read();
-    Serial.println(command);
+    // Serial.println(command);
     handleSerialCommand(command);
-    if ((prev_command == "u") || (prev_command == "d")){
-      nominal_current = current;
-    }
   }
 
-  if (isSlopeGreaterThan(current_readings, WINDOW_SIZE, 2.0)){
-    Serial.println("Препядствие!!!");
-    stopWindow();
-    delay(100);
-    lowerWindow();
-    delay(DELAY_OBSTACLE_LOWERING_WINDOW);
-  }
-
+// FAILURE DETECTION 
+  // if (isSlopeGreaterThan(current_readings, WINDOW_SIZE, SLOPE_THRESHOLD)){
+    // Serial.println("Препядствие!!!");
+    // stopWindow();
+    // delay(100);
+    // lowerWindow();
+    // delay(DELAY_OBSTACLE_LOWERING_WINDOW);
+  // }
+// ------------------
   if ((stateDOWN == 0) && (stateUP == 1)){
     if (prev_command == 'u'){
       stopWindow();
-      Serial.println("Не могу поднять окно...");
+      // Serial.println("Не могу поднять окно...");
     }
     if (prev_command == 'd'){
-      Serial.println("Невозможный сценарий");
+      // Serial.println("Невозможный сценарий");
     }
   }
   if ((stateDOWN == 1) && (stateUP == 0)){
     if (prev_command == 'd'){
       stopWindow();
-      Serial.println("Не могу опустить окно...");
+      // Serial.println("Не могу опустить окно...");
     }
     if (prev_command == 'u'){
-      Serial.println("Невозможный сценарий");
+      // Serial.println("Невозможный сценарий");
     }
   }
 
   if ((stateUP == 1) && (stateDOWN == 1)){
-    Serial.println("Неисправные сенсоры!");
+    // Serial.println("Неисправные сенсоры!");
   }
   if (stateUP == 1){
-    Serial.println("UP");
+    // Serial.println("UP");
   }
   if (stateDOWN == 1){
-    Serial.println("DOWN");
+    // Serial.println("DOWN");
   }
 
   delay(100);
@@ -124,21 +131,21 @@ void raiseWindow() {
   // Установить направление для поднятия окна (Реле 1: LOW для одного направления)
   digitalWrite(relayA, LOW);
   digitalWrite(relayB, HIGH);
-  Serial.println("Поднимаю окно...");
+  // Serial.println("Поднимаю окно...");
 }
 
 void lowerWindow() {
   // Установить направление для опускания окна (Реле 1: HIGH для противоположного направления)
   digitalWrite(relayA, HIGH);
   digitalWrite(relayB, LOW);
-  Serial.println("Опускаю окно...");
+  // Serial.println("Опускаю окно...");
 }
 
 void stopWindow() {
   // Отключить питание двигателя (Реле 1,2: ВЫКЛ)
   digitalWrite(relayA, LOW);
   digitalWrite(relayB, LOW);
-  Serial.println("Окно остановлено.");
+  // Serial.println("Окно остановлено.");
 }
 
 void addCurrentReading(float new_reading) {
